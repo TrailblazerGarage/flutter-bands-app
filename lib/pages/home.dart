b98fff06cef4d63f65d5c61a4cee77b8b734bff0
Jhony Vidal <jhony.oliveira@mulesoft.com>
2021-01-02 02:25:27 -0300

    Insert new band InputDialog Android/iOS functionality

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_band_name/models/band.dart';
 
@@ -24,13 +27,13 @@ class _HomePageState extends State<HomePage> {
         elevation: 1,
       ),
       body: ListView.builder(
-        itemCount: 4,
+        itemCount: bands.length,
         itemBuilder: (BuildContext context, int index) => _bandTile(bands[index])
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.add ),
         elevation: 1,
-        onPressed: () {}
+        onPressed: addNewBand
       ),
     );
   }
@@ -47,5 +50,65 @@ class _HomePageState extends State<HomePage> {
             print(band.name);
           }
         );
+  }
+
+  addNewBand() {
+    final TextEditingController textController = new TextEditingController();
+
+    if ( Platform.isAndroid ) {
+      return showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("New band name"),
+              content: TextField(
+                controller: textController,
+              ),
+              actions: <Widget>[
+                MaterialButton(
+                    child: Text('Add'),
+                    elevation: 5,
+                    textColor: Colors.blue,
+                    onPressed: () => addBandToList(textController.text)
+                )
+              ],
+            );
+          }
+      );
+    }
+
+    // iOS
+    showCupertinoDialog(
+        context: context,
+        builder: ( _ ) {
+          return CupertinoAlertDialog(
+            title: Text('New band name:'),
+            content: CupertinoTextField(
+              controller: textController,
+            ),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text('Add'),
+                onPressed: () => addBandToList( textController.text)
+              ),
+              CupertinoDialogAction(
+                  isDestructiveAction: true,
+                  child: Text('Dismiss'),
+                  onPressed: () => Navigator.pop(context)
+              )
+            ]
+          );
+        }
+    );
+  }
+
+  void addBandToList( String name ) {
+    if (name.length > 1) {
+      this.bands.add( new Band(id: DateTime.now().toString(), name: name, votes: 0));
+      setState(() {});
+    }
+
+    Navigator.pop(context);
   }
 }
